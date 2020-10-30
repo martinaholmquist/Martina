ee593c53634259a7e4251b595183b1cbb46247e4
marti <marti@DESKTOP-FMACCDF>
2020-10-30 11:29:46 +0100

    skickar mina första filer

---
+++ src/helloworld/Circle.java
@@ -0,0 +1,17 @@
+
+package helloworld;
+
+public class Circle {
+    double radius;
+    String color;
+    
+    
+    void display(){
+       String first = "Hello";
+       String second = "Students";
+       
+        System.out.println(" I am circle");
+        System.out.println(first + second);
+    }
+    
+}
---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        Circle circle1 = new Circle(); //detta är ett object och måste skapas 
+        //för att använda constructor som är det första order Circle
+
+        circle1.display();
+            
+    }
+
+}
